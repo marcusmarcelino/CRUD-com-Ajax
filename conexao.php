58f747d7437d78552a4694560b298d1aaa99a78b
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2019-10-25 14:54:53 -0300

    Arquivo conexao.php configurado e finalizado

--- conexao.php
+++ conexao.php
@@ -1,3 +1,13 @@
 <?php
+$hostname = "localhost";
+$user = "root";
+$password = "";
+$database = "mysqlDataManager";
 
+$conn = mysqli_connect ($hostname,$user,$password,$database);
+
+if(!$conn){
+   echo die("Falha na conexão".mysqli_connect_error());
+}else {echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!";
+}
 ?>
