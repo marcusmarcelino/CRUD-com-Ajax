32e2d7e060fde2f82bdb854e97ebea34b2c602bd
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2019-10-26 16:21:43 -0300

    Refatoramento de código

--- conexao.php
+++ conexao.php
@@ -8,9 +8,5 @@ $conn = mysqli_connect ($hostname,$user,$password,$database);
 
 if(!$conn){
    echo die("Falha na conexão".mysqli_connect_error());
-<<<<<<< HEAD
-}//else {echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!";}
-=======
 }
->>>>>>> functions-aux
 ?>
